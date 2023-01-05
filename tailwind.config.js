8b2fb87add7a41793d91b5d3feb9768a3b011c74
Amangoua Ange <adagou@yahoo.com>
2023-01-05 16:47:02 +0000

    asseets uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'arial'],
+      'body':[ "arial"]
+    },
+  },
+  plugins: [],
+}
